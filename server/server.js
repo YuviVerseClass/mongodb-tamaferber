6c621895c554c447312262e52ea6c0c992dc7918
Tama Ferber <tamaferber@gmail.com>
2025-06-27 13:21:18 +0300

    corrected everything. all is working well

--- server/server.js
+++ server/server.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const apiRoutes = require('./routes/api');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'client')));
 app.use('/api', apiRoutes);
 
-console.log("Connecting to:", process.env.MONGO_URL);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
